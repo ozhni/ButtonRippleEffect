ab93c997eb7e378015fc4e1cfc09544aa896e107
ozhni <mahsaqholami@gmail.com>
2024-03-01 19:47:54 +0330

    add method to btn

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event) => {
+  const x = event.pageX - btnEl.offsetLeft;
+  const y = event.pageY - btnEl.offsetTop;
+
+  btnEl.style.setProperty("--xpos", x + "px");
+  btnEl.style.setProperty("--ypos", y + "px");
+});
